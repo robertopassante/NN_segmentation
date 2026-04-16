eb462421075ff1bf6ac08e067ba3d7a9a684bd38
Roberto Passante <passante.1897160@studenti.uniroma1.it>
2026-04-16 22:18:39 +0200

    Increase MAX_TRAIN_SAMPLES from 2000 to 3000

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     DATASET_NAME = "openearthmap" # Opzioni: "loveda", "landcoverai", "deepglobe", "chesapeake", "openearthmap"
     
     # Subset: limita il numero di immagini per velocizzare il training (None = usa tutto)
-    MAX_TRAIN_SAMPLES = 2000
+    MAX_TRAIN_SAMPLES = 3000
     MAX_VAL_SAMPLES = 500
     
     # Adattamento Dinamico delle Classi
